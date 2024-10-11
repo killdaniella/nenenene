62a2221079b63b3f1d255e209f4b107ee2aa46c5
killdaniella <daniel.dzhemilev@gmail.com>
2024-10-11 10:08:37 +0300

    issand jumal
    aint 1 line oli chatgptga
    
    for (const [currency, rate] of Object.entries(data.rates)) {
    
    kuna ma ise poleks eales seda v2lja m6elnud

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-fetch('https://api-adresse.data.gouv.fr/reverse/?lon=2.37&lat=48.357')
+async function download() {
+    const url = 'https://open.er-api.com/v6/latest/USD'
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+
+    const element = document.querySelector('.pahn')
+    element.innerHTML = "";
+
+    for (const [currency, rate] of Object.entries(data.rates)) {
+        element.innerHTML += "<p>" + currency +": " + rate + "</p>"
+    }
 
 }
+  download()
+
+
+  
